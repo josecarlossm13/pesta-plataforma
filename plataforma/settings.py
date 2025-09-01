4663b91bce03dc314f257d9f0e8e9dee933d581f
JPolonia <joaovpsilva@hotmail.com>
2025-09-01 19:46:48 +0100

    Configure email settings in settings.py
    Added email configuration settings for SMTP.

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -357,3 +357,9 @@ EMAIL_BACKEND = os.environ.get(
     "EMAIL_BACKEND",
     "django.core.mail.backends.console.EmailBackend" if DEBUG else "django.core.mail.backends.smtp.EmailBackend"
 )
+EMAIL_HOST = os.environ.get("EMAIL_HOST",'smtp.gmail.com')
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER",'yoorusername@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD",'key') #past the key or password app here
+EMAIL_PORT = os.environ.get("EMAIL_PORT",587)
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS",True)
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL",'default from email')
